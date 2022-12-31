6aba50fbe479e531ab7f75dfc3f874b5bd143d29
Tolik <anatolijparygin988@gmail.com>
2022-12-31 15:21:42 +0300

    вывод чисел от N до 1 с помощью рекурсии

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void EnterNumber(int n)
+{
+    if (n == 0) return;
+    System.Console.Write(n + " ");
+    EnterNumber(n - 1);
+}
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+EnterNumber(number);
